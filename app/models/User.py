c6a16c8cc19a026306bfdb6cfb23b4429c1954f8
Mark T <aydenthos@gmail.com>
2023-10-31 12:47:59 -0700

    finished login and logout routing plus crypting

--- app/models/User.py
+++ app/models/User.py
@@ -3,25 +3,27 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import validates
 import bcrypt
 
-salt = bcrypt.gensalt()
-
 class User(Base):
-  __tablename__ = 'users'
-  id = Column(Integer, primary_key=True)
-  username = Column(String(50), nullable=False)
-  email = Column(String(50), nullable=False, unique=True)
-  password = Column(String(100), nullable=False)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
 
-  @validates('email')
-  def validate_email(self, key, email):
-    # make sure email address  contains @character
-    assert '@' in email
+    @validates('email')
+    def validate_email(self, key, email):
+        # Make sure email address contains @ character
+        assert '@' in email
+        return email
 
-    return email
-  
-  @validates('password')
-  def validate_password(self, key, password):
-    # make sure password is at least 5 characters long
-    assert len(password) > 4
+    @validates('password')
+    def validate_password(self, key, password):
+        # Make sure password is at least 5 characters long
+        assert len(password) > 4
+        # Hash the password with a unique salt
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
 
-    return bcrypt.hashpw(password.encode('utf-8'), salt)
+    def verify_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
