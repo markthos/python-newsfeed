76e5c6621506e35e6bfbcc27b5fa4a5570483b3d
Mark T <aydenthos@gmail.com>
2023-10-31 11:31:41 -0700

    setup signup routing and sessions

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -9,15 +10,27 @@ def signup():
     data = request.get_json()
     db = get_db()
     
-    # create new user
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+    try:
+        # attempt create new user
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
+        
+        # save user in database
+        db.add(newUser)
+        db.commit()
+    except:
+        # insert failed, so send error to front end
+        print(sys.exc_info()[0])
+
+        # insert failed, so rollback and send error to front end
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
     
-    # save in database
-    db.add(newUser)
-    db.commit()
+    session.clear()
+    session['user_id'] = newUser.id
+    session['loggedIn'] = True
     
     return jsonify(id=newUser.id)
